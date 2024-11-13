44f30ad651129168d19997649325774b65087c3d
shizu <nukuzihs@gmail.com>
2024-11-13 23:44:56 +0900

    debug for clickevent

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded' , function() {
-	const button = document.getElementById('goToIndex');
-
-	button.addEventListener('click',function() {
-		window.location.href = './index.html';
-	});
+// JavaScriptでボタンを取得してクリックイベントを設定
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('goToIndex');  // ボタンを取得
+    
+    button.addEventListener('click', function() {
+        window.location.href = './index.html';  // index.htmlに遷移
+    });
 });
