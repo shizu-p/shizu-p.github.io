865b2475d6a64f9f7faace8c75dfdeac64bbbc87
shizu <nukuzihs@gmail.com>
2024-11-14 10:24:19 +0900

    相対パスから絶対パスへ変更

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');  // <header>要素
     if(header){
 		header.addEventListener('click',function() {
-			window.location.href = './index.html';
+			window.location.href = 'https://shizu-p.github.io/index.html';
 		});
 	}
 });
